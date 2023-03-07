0317eef0dcc7f6142100c56564dc13d587e48334
miranda <126812087+miranfa@users.noreply.github.com>
2023-03-07 17:38:09 +0000

    Finished 7.1

---
+++ 7.1-arrays-intro.js
@@ -0,0 +1,20 @@
+function example() {
+  var topStudents = ["Christian", "Kristian", "Shakka", "Jaime", "AJ"];
+  var name = topStudents[2];
+  console.log(name);
+  topStudents[0] = "Mauricio";
+  console.log(topStudents);
+}
+
+function problem1() {
+  var travelList = ["Paris", "London", "Alaska", "Canada", "New York"];
+  var place = travelList[2];
+  console.log(place);
+}
+
+
+function problem2(){
+  var numList = [2, 3, 5, 7, 11];
+  var nums = numList[0];
+  console.log(nums);
+}
